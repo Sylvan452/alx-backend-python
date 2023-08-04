08050c83ff6d6cce1f01db2b57bf195f7581c36d
sylvan452 <sylvan452@gmail.com>
2023-08-04 17:12:47 +0000

    task 4 corrected

--- 0x00-python_variable_annotations/4-define_variables.py
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Thia Module contains definition of variables with specified values and annotations."""
+"""Module contains definition of specified var values and annotations."""
 
 a: int = 1
 pi: float = 3.14
