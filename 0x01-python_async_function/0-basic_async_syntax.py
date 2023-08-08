501d0246e0c713a4557c754889672ca4bc594ddd
sylvan452 <sylvan452@gmail.com>
2023-08-08 10:10:44 +0000

    white space removed

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -8,7 +8,7 @@ import random
 
 async def wait_random(max_delay: int = 10) -> float:
     """
-    it takes an integer - max_delay, waits for a random delay between 
+    it takes an integer - max_delay, waits for a random delay between
     0 and max_delay seconds and then returns the random int.
     """
     random_delay = random.uniform(0, max_delay)
